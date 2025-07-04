86d276a45eaec8f0766c69c14de6609a728728c0
Sean Nieves <202311382@fit.edu.ph>
2025-07-04 13:55:23 +0800

    working camera, player positioned to the left

--- Oblivion/Camera.cs
+++ Oblivion/Camera.cs
@@ -20,34 +20,38 @@ namespace Oblivion
         {
             Vector2 targetPos = new Vector2(
             target.Player_Position.X + (target.Player_Rectangle.Width / 2),
-            target.Player_Position.Y + (target.Player_Rectangle.Height / 2) - 50
-    );
+            target.Player_Position.Y + (target.Player_Rectangle.Height / 2) - 50);
+            Console.Write(target.Player_Position.X);
+            float cameraX = targetPos.X - Game1.screenWidth * .15f;
+            float cameraY = targetPos.Y - Game1.screenHeight / 2f;
 
-            // Calculate camera's intended position
-            float cameraX = targetPos.X - Game1.screenWidth/2f;
-            float cameraY = targetPos.Y - Game1.screenHeight/2f;
-
-            // Clamp camera to the wallpaper bounds
-            // if (target.Player_Position.X < 640)
-            // {
-                cameraX = MathHelper.Clamp(cameraX, 0, 523 - target.Player_Rectangle.Width);
-            
-            //}
-            // else
-            // {
-            //     Console.WriteLine("DEBUG");
-            //     cameraX = MathHelper.Clamp(cameraX, 640, 1280);
-            // }
-
+            cameraX = MathHelper.Clamp(cameraX, 0, 525 - target.Player_Rectangle.Width);
             cameraY = MathHelper.Clamp(cameraY, 0, 720);
 
-            _position = new Vector2(cameraX, cameraY); // Save this if you want to reference position
+            _position = new Vector2(cameraX, cameraY);
 
             // Create the camera transform matrix
             Transform = Matrix.CreateTranslation(-_position.X, -_position.Y, 0f) *
                         Matrix.CreateScale(zoom) *
                         Matrix.CreateTranslation(0, 0, 0);
+                        
+            //V2 WORKS
+            // Vector2 targetPos = new Vector2(
+            //             target.Player_Position.X + (target.Player_Rectangle.Width / 2),
+            //             target.Player_Position.Y + (target.Player_Rectangle.Height / 2) - 50
+            //     );
+            //             Console.Write(target.Player_Position.X);
+            //             float cameraX = targetPos.X - Game1.screenWidth/2f;
+            //             float cameraY = targetPos.Y - Game1.screenHeight/2f;
 
+            //             cameraX = MathHelper.Clamp(cameraX, 0, 525 - target.Player_Rectangle.Width);
+            //             cameraY = MathHelper.Clamp(cameraY, 0, 720);
+
+            //             _position = new Vector2(cameraX, cameraY);
+
+            //             Transform = Matrix.CreateTranslation(-_position.X, -_position.Y, 0f) *
+            //                         Matrix.CreateScale(zoom) *
+            Matrix.CreateTranslation(0, 0, 0);
 
             //JUST IN CASE SHIT DOESNT WORK
             //      var position = Matrix.CreateTranslation(-target.Player_Position.X - (target.Player_Rectangle.Width / 2),
